b79548971466b335b903c907685b49cd9f295486
daikikojima <daikikojima0531@icloud.com>
2019-07-03 07:39:11 +0900

    F: some style

--- pages/about.vue
+++ pages/about.vue
@@ -4,52 +4,75 @@
       <h1 class="title">
         about me...
       </h1>
-      <section class="section-box">
-        <h2>Career</h2>
+      <terminal-box>
+        <p>$ show Career</p>
         <ul>
-          <li class="font-sans text-lg">2014.4 ~ 2018.3: Kyoto Univ, Fac of Engineering, Dept of Informatics And Applud Math & Physics</li>
-          <li class="font-sans text-lg">2017.4 ~ 2018.3: Studied @ Information System Group</li>
-          <li class="font-sans text-lg">2019.4 ~ Now: Kyoto Univ, Grad School of Informatics, Dept of System Science</li>
-          <li class="font-sans text-lg">2019.4 ~ Now: Studying @ Nakashima lab (Applied Informatics Group)</li>
+          <li>2014.4 ~ 2018.3: Kyoto Univ, Fac of Engineering, Dept of Informatics</li>
+          <li>2017.4 ~ 2018.3: Studied @ Information System Group</li>
+          <li>2019.4 ~ Now: Kyoto Univ, Grad School of Informatics, Dept of System Science</li>
+          <li>2019.4 ~ Now: Studying @ Nakashima lab (Applied Informatics Group)</li>
         </ul>
-      </section>
+      </terminal-box>
+      <terminal-box>
+        <p>$ show Skill</p>
+        <ul>
+          <li>C/C++</li>
+          <li>JavaScript/TypeScript</li>
+          <ul>
+            <li>Node.js</li>
+            <li>Vue.js (Vuex)</li>
+            <li>React</li>
+          </ul>
+          <li>Java/Kotlin</li>
+          <ul>
+            <li>Android apps</li>
+            <li>Spring boot (a little bit)</li>
+          </ul>
+          <li>Swift (iOS dev)</li>
+          <li>Ruby (on Rails)</li>
+          <li>Python</li>
+          <ul>
+            <li>Data visualization</li>
+            <li>Web scraping</li>
+            <li>Web (Django)</li>
+          </ul>
+        </ul>
+      </terminal-box>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import TerminalBox from '~/components/TerminalBox.vue'
 
 export default {
   components: {
-    Logo
+    TerminalBox
   }
 }
 </script>
 
-<style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss" scoped>
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.section-box {
-  width: 100%;
-  background: #00C48D;
-
-}
+  li {
+    list-style: none;
+    margin: 2% 0;
+  }
 </style>
